0511d06593ad476c6d33e4b53a615359a93a5990
Dandi Lesmana <98586002+DandiLesmana25@users.noreply.github.com>
2025-10-26 17:00:29 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
-CREATE TABLE db_name (
-    id int NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255),
     email varchar(255),
     age int
